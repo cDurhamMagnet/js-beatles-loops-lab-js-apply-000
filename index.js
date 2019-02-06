a870f0bdc69bb91721b7fdea8a47e13ae632de1a
Christopher Durham <c.durham@protonmail.com>
2019-02-06 19:39:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  let result = []
+  for(let i = 0; i < musicians.length; i++){
+    result[i] = musicians[i] + "plays " + instruments[i]
+  }
+  return result
+}
+
+function johnLennonFacts(facts){
+  let result = []
+  let i = 0;
+  while(i < facts.lenght){
+    result[i] = facts[i] + "!!!"
+    i++;
+  }
+  return result
+}
+
+function iLoveTheBeatles(number){
+  let result = []
+  let i = 0;
+  do {
+    result[i] = "I love the Beatles!";
+    i++;
+  } while (i < number)
+  
+  
+  return result
+}
